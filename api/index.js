8cc1c339b26b43bdcf129ab0ee408bd11a34e358
trh-thanh30 <tranhuuthanhcp@gmail.com>
2024-07-17 23:05:00 +0700

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,11 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("Error", err));
 const app = express();
 app.listen(3000, () => {
   console.log("Sever is running on port 3000!");
