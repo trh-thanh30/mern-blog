3c55f54172335dedcfa0f810f00a2b7458bda9ff
trh-thanh30 <tranhuuthanhcp@gmail.com>
2024-07-19 16:14:15 +0700

    complete sidebar of the dash

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,5 +1,26 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashProfile from "../components/DashProfile";
+import DashSideBar from "../components/DashSideBar";
 export default function DashboardPage() {
-    return (
-        <h1>Dashboard</h1>
-    )
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFormUrl = urlParams.get("tab");
+    if (tabFormUrl) {
+      setTab(tabFormUrl);
+    }
+  }, [location.search]);
+  return (
+    <div className="flex flex-col min-h-screen md:flex-row">
+      <div className="md:w-56">
+        {/* Sidebar */}
+        <DashSideBar></DashSideBar>
+      </div>
+
+      {/* Profile... */}
+      {tab === "profile" && <DashProfile></DashProfile>}
+    </div>
+  );
 }
