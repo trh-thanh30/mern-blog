636bf1f85b3ef13a7278f863a5c4a73c6752b5cb
trh-thanh30 <tranhuuthanhcp@gmail.com>
2024-07-21 15:48:58 +0700

    create get posts api route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
-import { create } from "../controllers/post.controller.js";
+import { create, getPosts } from "../controllers/post.controller.js";
 const route = express.Router();
 route.post("/create", verifyToken, create);
-
+route.get("/getposts", getPosts);
 export default route;
